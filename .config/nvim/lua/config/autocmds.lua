8ba73c4c14ac87b9530ed2a5933c1780d0ed7974
Bells307 <sdikansky96@gmail.com>
2025-04-04 17:20:08 +0300

    upd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,2 +1,9 @@
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    require("conform").format({ async = true, lsp_fallback = true })
+  end,
+})
